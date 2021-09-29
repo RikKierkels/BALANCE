5c57969f4a758f3c7d58edf61c55e6dc9b5ceb95
Rik Kierkels <kierkels.r@gmail.com>
2021-09-29 11:17:42 +0200

    Add a functional pipe

--- balance/src/common/index.ts
+++ balance/src/common/index.ts
@@ -9,6 +9,14 @@ export const compareToKeepOne =
   (xs: T[]): T =>
     xs.reduce((prev, curr) => (compareFn(prev, curr) ? curr : prev));
 
+export const pipe = <T extends any[], R>(fn1: (...args: T) => R, ...fns: Array<(a: R) => R>) => {
+  const piped = fns.reduce(
+    (prevFn, nextFn) => (value: R) => nextFn(prevFn(value)),
+    (value) => value,
+  );
+  return (...args: T) => piped(fn1(...args));
+};
+
 export const diff = (a: number, b: number) => a - b;
 
 export const isEmpty = <T>(xs: T[]): boolean => !xs?.length;
--- balance/src/portfolio/portfolio-balancer.ts
+++ balance/src/portfolio/portfolio-balancer.ts
@@ -1,36 +1,47 @@
 import { Fund, Portfolio } from "./portfolio";
-import { compareToKeepOne, diff, isEmpty, repeat } from "../common";
+import { compareToKeepOne, diff, isEmpty, pipe, repeat } from "../common";
 
 const weightGap = ({ weight }: Fund) => diff(weight.target, weight.actual);
 const weightGapComparer = (a: Fund, b: Fund) => weightGap(a) < weightGap(b);
 const fundWithLargestWeightGap = compareToKeepOne(weightGapComparer);
+
 const isAffordable =
   (amount: number) =>
   ({ price }: Fund) =>
     price <= amount;
+
 const updateWeight = (total: number) => (fund: Fund) => ({
   ...fund,
   weight: { ...fund.weight, actual: (fund.quantity * fund.price) / total },
 });
-const updateWeights = ({ funds, total }: Portfolio): Portfolio => ({ funds: funds.map(updateWeight(total)), total });
+
+const updateWeights = ({ funds, total }: Portfolio): Portfolio => ({
+  funds: funds.map(updateWeight(total)),
+  total,
+});
+
 const increaseQuantity =
   (by: number) =>
   (fund: Fund): Fund => ({ ...fund, quantity: fund.quantity + by });
 const increaseQuantityByOne = increaseQuantity(1);
 
+const updateQuantityOf =
+  ({ id, price }: Fund) =>
+  ({ funds, total }: Portfolio): Portfolio => ({
+    funds: funds.map((fund) => (fund.id === id ? increaseQuantityByOne(fund) : fund)),
+    total: total + price,
+  });
+
 const _balance =
   (amount: number) =>
-  ({ funds, total }: Portfolio): Portfolio => {
-    const affordableFunds = funds.filter(isAffordable(amount));
-    if (isEmpty(affordableFunds)) return { funds, total };
+  (portfolio: Portfolio): Portfolio => {
+    const affordableFunds = portfolio.funds.filter(isAffordable(amount));
+    if (isEmpty(affordableFunds)) return portfolio;
 
-    const { id, price } = fundWithLargestWeightGap(affordableFunds);
-    const portfolio: Portfolio = {
-      funds: funds.map((fund) => (fund.id === id ? increaseQuantityByOne(fund) : fund)),
-      total: total + price,
-    };
+    const fundToBuy = fundWithLargestWeightGap(affordableFunds);
+    const balancePortfolio = pipe(updateQuantityOf(fundToBuy), updateWeights);
 
-    return _balance(amount - price)(updateWeights(portfolio));
+    return _balance(amount - fundToBuy.price)(balancePortfolio(portfolio));
   };
 
 const balance = (portfolio: Portfolio, amount: number, times: number) => repeat(times)(_balance(amount))(portfolio);
