9d4ea4bb9edb07550bf590c9888770224dde7e70
Rik Kierkels <kierkels.r@gmail.com>
2022-03-15 13:16:53 +0100

    Add an error icon to input validation errors

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { ReactComponent as WarningIcon } from "../../assets/warning.svg";
 
 type InputProps = React.ComponentPropsWithoutRef<"input">;
 type Props = React.PropsWithChildren<{ error?: string } & InputProps>;
@@ -8,7 +9,12 @@ const Input = React.forwardRef<HTMLInputElement, Props>(({ children, className, 
   <StyledLabel className={className}>
     <span>{children}</span>
     <StyledInput ref={ref} aria-invalid={!!error ? "true" : "false"} {...props} />
-    {!!error && <Error role="alert">{error}</Error>}
+    {!!error && (
+      <Error>
+        <ErrorIcon />
+        <ErrorMessage role="alert">{error}</ErrorMessage>
+      </Error>
+    )}
   </StyledLabel>
 ));
 
@@ -29,9 +35,24 @@ const StyledInput = styled.input`
   border: 1px solid ${({ theme }) => theme.colors.input.border};
 `;
 
-const Error = styled.span`
+const Error = styled.div`
+  display: inline-flex;
+  align-items: center;
+
+  > *:first-child {
+    margin-inline-end: 0.5em;
+  }
+`;
+
+const ErrorMessage = styled.span`
   color: ${({ theme }) => theme.colors.input.error};
-  font-size: ${({ theme }) => theme.font.xs};
+  font-size: ${({ theme }) => theme.font.sm};
+`;
+
+const ErrorIcon = styled(WarningIcon)`
+  height: 0.75em;
+  width: 0.75em;
+  fill: ${({ theme }) => theme.colors.input.error};
 `;
 
 export default Input;
